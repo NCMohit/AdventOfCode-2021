1b07facf9fc85f6b56e77fd64d340bc3aa8e3bff
NCMohit <mohitnc18@gmail.com>
2021-12-11 11:57:59 +0530

    Day 11 solutions

---
+++ 2021/Day11/prob1.py
@@ -0,0 +1,81 @@
+matrix = []
+file = open("input.txt","r")
+for line in file.readlines():
+    row = []
+    for i in line.strip():
+        row.append(int(i))
+    matrix.append(row)
+
+flashes = 0
+for step in range(100):
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+            matrix[row][col] += 1
+    flashed_points = []
+    noflashes = 0
+    while(noflashes == 0):
+        noflashes = 1
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                if([row,col] not in flashed_points):
+                    if(matrix[row][col]>=10):
+                        noflashes = 0
+                        flashed_points.append([row,col])
+                        if(row == 0):
+                            if(col == 0):
+                                matrix[row+1][col] += 1
+                                matrix[row][col+1] += 1
+                                matrix[row+1][col+1] += 1
+                            elif(col == (len(matrix[0])-1)):
+                                matrix[row+1][col-1] += 1
+                                matrix[row+1][col] += 1
+                                matrix[row][col-1] += 1
+                            else:
+                                matrix[row+1][col] += 1
+                                matrix[row][col-1] += 1
+                                matrix[row+1][col-1] += 1
+                                matrix[row][col+1] += 1
+                                matrix[row+1][col+1] += 1
+                        elif(row == (len(matrix)-1)):
+                            if(col == 0):
+                                matrix[row-1][col] += 1
+                                matrix[row][col+1] += 1
+                                matrix[row-1][col+1] += 1
+                            elif(col == (len(matrix[0])-1)):
+                                matrix[row-1][col-1] += 1
+                                matrix[row-1][col] += 1
+                                matrix[row][col-1] += 1
+                            else:
+                                matrix[row-1][col] += 1
+                                matrix[row][col-1] += 1
+                                matrix[row-1][col-1] += 1
+                                matrix[row][col+1] += 1
+                                matrix[row-1][col+1] += 1
+                        else:
+                            if(col == 0):
+                                matrix[row-1][col] += 1
+                                matrix[row][col+1] += 1
+                                matrix[row-1][col+1] += 1
+                                matrix[row+1][col+1] += 1
+                                matrix[row+1][col] += 1
+                            elif(col == (len(matrix[0])-1)):
+                                matrix[row-1][col-1] += 1
+                                matrix[row-1][col] += 1
+                                matrix[row][col-1] += 1
+                                matrix[row+1][col-1] += 1
+                                matrix[row+1][col] += 1
+                            else:
+                                matrix[row-1][col] += 1
+                                matrix[row][col-1] += 1
+                                matrix[row-1][col-1] += 1
+                                matrix[row][col+1] += 1
+                                matrix[row-1][col+1] += 1  
+                                matrix[row+1][col] += 1
+                                matrix[row+1][col-1] += 1 
+                                matrix[row+1][col+1] += 1
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+            if(matrix[row][col]>=10):
+                matrix[row][col] = 0
+                flashes += 1
+print("Flashes: ",flashes)       
